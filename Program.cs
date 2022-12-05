743735da17b54d35f0e984a443dd618c55e0bb35
Morgan Hofmann <morgan@theopensource.company>
2022-12-05 13:39:51 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Celestia
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game())
+                game.Run();
+        }
+    }
+}
