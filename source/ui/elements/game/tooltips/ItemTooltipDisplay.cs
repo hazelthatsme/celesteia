660b4a50ce59a563feb7bff48131b009cfb8fd1a
Morgan Hofmann <morgan@theopensource.company>
2023-03-15 09:34:01 +0100

    Remove unused lore from item tooltip

--- source/ui/elements/game/tooltips/ItemTooltipDisplay.cs
+++ source/ui/elements/game/tooltips/ItemTooltipDisplay.cs
@@ -10,21 +10,20 @@ namespace Celesteia.UI.Elements.Game.Tooltips {
         public readonly Container Content;
         public readonly Label Title;
         public readonly ItemDisplay Item;
-        public readonly Label Lore;
 
         public ItemTooltipDisplay(Rect rect, Texture2D background) : base(rect) {
             AddChild(new Image(Rect.RelativeFull(new Rect(
                 AbsoluteUnit.WithValue(0f),
                 AbsoluteUnit.WithValue(0f),
                 AbsoluteUnit.WithValue(256f + (2 * OUTER_SPACING)),
-                AbsoluteUnit.WithValue(64f + (1 * INNER_SPACING) + (2 * OUTER_SPACING))
+                AbsoluteUnit.WithValue(32f + (2 * OUTER_SPACING))
             ))).SetTexture(background).MakePatches(4).SetColor(Color.White));
 
             Content = new Container(new Rect(
                 AbsoluteUnit.WithValue(OUTER_SPACING),
                 AbsoluteUnit.WithValue(OUTER_SPACING),
                 AbsoluteUnit.WithValue(256f),
-                AbsoluteUnit.WithValue(64f + (1 * INNER_SPACING))
+                AbsoluteUnit.WithValue(32f)
             ));
 
             Container titleCard = new Container(new Rect(
@@ -47,18 +46,6 @@ namespace Celesteia.UI.Elements.Game.Tooltips {
             )).SetTextProperties(new Properties.TextProperties().Standard().SetTextAlignment(TextAlignment.Left)).SetPivotPoint(new Vector2(0f, 0f)));
             Content.AddChild(titleCard);
 
-            Container lore = new Container(new Rect(
-                new RelativeUnit(.5f, Content.GetRect(), RelativeUnit.Orientation.Horizontal),
-                AbsoluteUnit.WithValue(32f + INNER_SPACING),
-                new RelativeUnit(1f, Content.GetRect(), RelativeUnit.Orientation.Horizontal),
-                AbsoluteUnit.WithValue(32f)
-            ));
-            lore.AddChild(Lore = new Label(Rect.RelativeFull(lore.GetRect())).SetTextProperties(new Properties.TextProperties().Standard()
-                .SetFontSize(16f)
-            ).SetPivotPoint(new Vector2(0.5f, 0f)));
-            
-            Content.AddChild(lore);
-
             AddChild(Content);
 
             SetEnabled(false);
@@ -67,7 +54,6 @@ namespace Celesteia.UI.Elements.Game.Tooltips {
         public void SetItem(ItemType type) {
             Item.SetItem(type);
             Title.SetText(type.Name);
-            Lore.SetText(type.Lore);
         }
     }
 }
